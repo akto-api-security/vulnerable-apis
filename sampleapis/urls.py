1eb6da3fe91a6daec1ad7460285ea9545c5898d3
Rishav1919 <rishav@akto.io>
2023-06-10 12:45:04 +0530

    Added api for trace,track and server version exposed method test

--- sampleapis/urls.py
+++ sampleapis/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("echo/", views.echo, name="echo"),
+    path("track/", views.track, name="track"),
+    path("serverversion/", views.serverversion, name="serverversion"),
 ]
 
--- sampleapis/views.py
+++ sampleapis/views.py
@@ -6,7 +6,39 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 
+
+@api_view(['GET', 'TRACE'])
+def index(request):
+    if request.method == 'GET':
+        return HttpResponse("WELCOME !!")
+    elif request.method == 'TRACE':
+        response = HttpResponse("WELCOME !!", status=200)
+        response[
+            'Custom_Header_Name'] = 'Custom_Header_Value'  # here we can add the custom header which will be used in validating the test
+        return response
+    else:
+        return HttpResponse("Method not allowed", status=405)
+
+    
+@api_view(['POST', 'TRACK'])
+def track(request):
+    if request.method == 'POST':
+        return HttpResponse("WELCOME !!")
+    elif request.method == 'TRACK':
+        response = HttpResponse("WELCOME !!", status=200)
+        response['Custom_Header_Name'] = 'Custom_Header_Value'  # here we can add the custom header which will be used in validating the test
+        return response
+    else:
+        return HttpResponse("Method not allowed", status=405)
+
+
+@api_view(['GET', 'POST'])
+def serverversion(request):
+    response = HttpResponse("WELCOME !!")
+   # here we can add the server details in header for example we can use(Apache/2.4.18 (Ubuntu) or (nginx/1.18.0) or (Express/4.17.1))
+    # response['Server'] = 'Server Details'
+    return response
+
 @api_view(['GET'])
 def echo(request):
     return HttpResponse(json.dumps(request.data), status=200)
-
