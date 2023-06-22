2c6cdbfaf7601826089a8d97346aa4e1cc3ef3e6
Rishav1919 <rishav@akto.io>
2023-06-22 15:28:33 +0530

    all ssrf api completed

--- sampleapis/urls.py
+++ sampleapis/urls.py
@@ -14,7 +14,12 @@ urlpatterns = [
     path("academics/courses/v2/registration/payment/", views.payment_v2, name="registration"), #Old Api Version Vulnerability
     path("dashboard/attendence/", views.getAttendence, name="profile-details"), #Content Type Header Missing Vulnerability
     path("profile/edit-details/upload-transcript/", views.upload_transcript, name="upload_transcript"), #SSRF on CSV upload
-    path("profile/edit-details/upload-profile-image/", views.upload_profile_pic, name="upload_profile_pic"),
-    path("profile/edit-details/upload-resume/", views.upload_resume, name="upload_resume"),
+    path("profile/edit-details/upload-profile-image/", views.upload_profile_pic, name="upload_profile_pic"), #SSRF on Image upload
+    path("profile/edit-details/upload-resume/", views.upload_resume, name="upload_resume"), #SSRF on PDF upload
+    path("academics/curriculum/upload-lab-reports/", views.upload_lab_report, name="upload_lab_report"), #SSRF on XML upload
+    path("home/search/", views.search_portal, name="search_website"), #SSRF on AWS Meta Endpoint
+    path("home/recents/", views.getRecents, name="recents"), #SSRF on Localhost
+    path("home/tnp-notifications/view/", views.getTnp_notifications, name="tnp_notifications"), #SSRF on Lcalhost DNS Pinning
+    path("profile/documents/view/", views.getDocuments, name="view_documents"), #SSRF on Files
 ]
 
--- sampleapis/views.py
+++ sampleapis/views.py
@@ -52,11 +52,14 @@ def getResults(request):
 @api_view(['GET', 'POST'])
 def getFeedbacks(request):
     url = request.GET.get('url')
-    # print(url)
-    response = HttpResponse()
-    response['Location'] = url
-    response.status_code = 302
-    return response
+    if url:
+        response = HttpResponse()
+        response['Location'] = url
+        response.status_code = 302
+        return response
+    else:
+        Data = fetch_file('sampleapis/Data/user_feedbacks.json')
+        return JsonResponse(Data, safe=False)
 
 @api_view(['GET'])
 def getCourseList(request):
@@ -119,7 +122,7 @@ def upload_transcript(request):
             'instance-action': 'pending',
         }
         return JsonResponse(res_data,safe=False)
-    elif(csv_file_value):
+    elif(csv_file_value and csv_file_value.endswith('.csv') and csv_file_value != '.csv'):
         return JsonResponse({'message': 'file uploaded successfully'})
     else:
         return JsonResponse({'error':'something went wrong(check file type)'},status=500)
@@ -159,7 +162,7 @@ def upload_profile_pic(request):
             'instance-action': 'pending',
         }
         return JsonResponse(res_data,safe=False)
-    elif(img_file_value):
+    elif(img_file_value and img_file_value.endswith('.jpg') and img_file_value != '.jpg'):
         return JsonResponse({'message': 'Profile Image uploaded successfully'})
     else:
         return JsonResponse({'error':'something went wrong(check file type)'},status=500)
@@ -183,11 +186,167 @@ def upload_resume(request):
             'instance-action': 'pending',
         }
         return JsonResponse(res_data,safe=False)
-    elif(pdf_file_value):
+    elif(pdf_file_value and pdf_file_value.endswith('.pdf') and pdf_file_value != '.pdf'):
         return JsonResponse({'message': 'PDF uploaded successfully'})
     else:
         return JsonResponse({'error':'something went wrong(check file type)'},status=500)  
 
+
+@api_view(['POST'])
+def upload_lab_report(request):
+    xml_file_value = request.GET.get('xml_file')
+    
+    if xml_file_value == 'https://ssrf.localdomain.pw/xml-without-body-md/301-http-.x.xml':
+        res_data = {
+            'ami-id': 'some-ami-id-value',
+            'ami-launch-index': '1',
+            'ami-manifest-path': '/path/to/manifest',
+            'block-device-mapping': '/dev/sda1',
+            'instance-action': 'pending',
+        }
+        return JsonResponse(res_data,safe=False)
+    elif(xml_file_value and xml_file_value.endswith('.xml') and xml_file_value != '.xml'):
+        return JsonResponse({'message': 'XML file uploaded successfully'})
+    else:
+        return JsonResponse({'error':'something went wrong(check file type)'},status=500)
+
+
+@api_view(['GET'])
+def search_portal(request):
+    url = request.GET.get('url')
+    if url == 'http://169.254.169.254/latest/meta-data/local-ipv4':
+        res_data = {
+            'ami-id': 'some-ami-id-value',
+            'ami-launch-index': '1',
+            'ami-manifest-path': '/path/to/manifest',
+            'block-device-mapping': '/dev/sda1',
+            'instance-action': 'pending',
+        }
+        return JsonResponse(res_data,safe=False)
+    elif url == 'http://⑯⑨。②⑤④。⑯⑨｡②⑤④/latest/meta-data/local-ipv4':
+        res_data = {
+            'ami-id': 'some-ami-id-value',
+            'ami-launch-index': '1',
+            'ami-manifest-path': '/path/to/manifest',
+            'block-device-mapping': '/dev/sda1',
+            'instance-action': 'pending',
+        }
+        return JsonResponse(res_data,safe=False)
+    elif  url:
+        response = requests.get(url)
+        data = response.text
+        return JsonResponse({'data': data})
+    else:
+        return JsonResponse({'error':'something went wrong'}, status=500)
+
+# @api_view(['GET'])
+# def vulnerable_view_1(request):
+#     url = request.GET.get('url')
+#     if url =='http://⑯⑨。②⑤④。⑯⑨｡②⑤④/latest/meta-data/local-ipv4':
+#         res_data = {
+#             'ami-id': 'some-ami-id-value',
+#             'ami-launch-index': '1',
+#             'ami-manifest-path': '/path/to/manifest',
+#             'block-device-mapping': '/dev/sda1',
+#             'instance-action': 'pending',
+#         }
+#         return JsonResponse(res_data,safe=False)
+#     elif  url:
+#         response = requests.get(url)
+#         data = response.text
+#         return JsonResponse({'data': data})
+#     else:
+#         return JsonResponse({'error':'something went wrong'}, status=500)
+
+
+@api_view(['GET'])
+def getRecents(request):
+    url = request.GET.get('url')
+    if url =='http://localhost/admin':
+        res_data = {
+            "status": "success",
+            "message": "Welcome to the Admin Panel",
+            "data": {
+                "user_count": 150,
+                "last_login": "2023-06-19 15:30:45",
+                "privileged_accounts": ["admin", "superuser"],
+                "server_status": "Online"
+            }
+        }
+        return JsonResponse(res_data,safe=False)
+    elif url == 'http://①②⑦。⓪⓪⓪。⓪⓪①/admin':
+        res_data = {
+            "status": "success",
+            "message": "Welcome to the Admin Panel",
+            "data": {
+                "user_count": 150,
+                "last_login": "2023-06-19 15:30:45",
+                "privileged_accounts": ["admin", "superuser"],
+                "server_status": "Online"
+            }
+        }
+        return JsonResponse(res_data,safe=False)
+    elif  url:
+        response = requests.get(url)
+        data = response.text
+        return JsonResponse({'data': data})
+    else:
+        return JsonResponse({'error':'something went wrong'}, status=500)
+
+# @api_view(['GET'])
+# def vulnerable_view_3(request):
+#     url = request.GET.get('url')
+#     if url == 'http://①②⑦。⓪⓪⓪。⓪⓪①/admin':
+#         res_data = {
+#             "status": "success",
+#             "message": "Welcome to the Admin Panel",
+#             "data": {
+#                 "user_count": 150,
+#                 "last_login": "2023-06-19 15:30:45",
+#                 "privileged_accounts": ["admin", "superuser"],
+#                 "server_status": "Online"
+#             }
+#         }
+#     elif  url:
+#         response = requests.get(url)
+#         data = response.text
+#         return JsonResponse({'data': data})
+#     else:
+#         return JsonResponse({'error':'something went wrong'}, status=500)
+
+
+@api_view(['GET'])
+def getTnp_notifications(request):
+    url = request.GET.get('url')
+    if url == 'http://make-127-0-0-1-rr.1u.ms/admin':
+        res_data = {
+            "status": "success",
+            "message": "Welcome to the Admin Panel",
+            "data": {
+                "user_count": 150,
+                "last_login": "2023-06-19 15:30:45",
+                "privileged_accounts": ["admin", "superuser"],
+                "server_status": "Online"
+            }
+        }
+        return JsonResponse(res_data,safe=False)
+    elif  url:
+        response = requests.get(url)
+        data = response.text
+        return JsonResponse({'data': data})
+    else:
+        return JsonResponse({'error':'something went wrong'}, status=500)
+
+@api_view(['GET'])
+def getDocuments(request):
+    file = request.GET.get('file')
+    if file == '///etc/passwd':
+        res_data = {'Data':'root:x:0:0:root:/root:/bin/ash\nbin:x:1:1:bin:/bin:/sbin/nologin\ndaemon:x:2:2:daemon:/sbin:/sbin/nologin\nadm:x:3:4:adm:/var/adm:/sbin/nologin\nlp:x:4:7:lp:/var/spool/lpd:/sbin/nologin\nsync:x:5:0:sync:/sbin:/bin/sync\nshutdown:x:6:0:shutdown:/sbin:/sbin/shutdown\nhalt:x:7:0:halt:/sbin:/sbin/halt\nmail:x:8:12:mail:/var/mail:/sbin/nologin\nnews:x:9:13:news:/usr/lib/news:/sbin/nologin\nuucp:x:10:14:uucp:/var/spool/uucppublic:/sbin/nologin\noperator:x:11:0:operator:/root:/sbin/nologin\nman:x:13:15:man:/usr/man:/sbin/nologin\npostmaster:x:14:12:postmaster:/var/mail:/sbin/nologin\ncron:x:16:16:cron:/var/spool/cron:/sbin/nologin\nftp:x:21:21::/var/lib/ftp:/sbin/nologin\nsshd:x:22:22:sshd:/dev/null:/sbin/nologin\nat:x:25:25:at:/var/spool/cron/atjobs:/sbin/nologin\nsquid:x:31:31:Squid:/var/cache/squid:/sbin/nologin\nxfs:x:33:33:X Font Server:/etc/X11/fs:/sbin/nologin\ngames:x:35:35:games:/usr/games:/sbin/nologin\ncyrus:x:85:12::/usr/cyrus:/sbin/nologin\nvpopmail:x:89:89::/var/vpopmail:/sbin/nologin\nntp:x:123:123:NTP:/var/empty:/sbin/nologin\nsmmsp:x:209:209:smmsp:/var/spool/mqueue:/sbin/nologin\nguest:x:405:100:guest:/dev/null:/sbin/nologin\nnobody:x:65534:65534:nobody:/:/sbin/nologin\nnginx:x:100:101:nginx:/var/lib/nginx:/sbin/nologin\nvnstat:x:101:102:vnstat:/var/lib/vnstat:/bin/false\nredis:x:102:103:redis:/var/lib/redis:/bin/false'}
+        return JsonResponse(res_data,safe=False)
+    elif  file:
+        return JsonResponse({'message': 'you are trying to access files'})
+    else:
+        return JsonResponse({'error':'something went wrong'}, status=500)
 
 @api_view(['GET'])
 def echo(request):
