f8984c404da82be548cd254f2e6166e76b16234a
Rishav1919 <rishav@akto.io>
2023-07-13 21:45:22 +0530

    shifted database to postgres

--- vulnerableapis/settings.py
+++ vulnerableapis/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'vulnerableapis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'abc@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     },
     'mongo': {
         'ENGINE': 'djongo',
